1b97b31c287d1476e475fd464053bb263ddc3515
Atul9 <atul1bhosale@gmail.com>
2014-06-30 06:11:45 +0530

    Removed old files

--- 3e_how_old.rb
+++
@@ -1,6 +0,0 @@
-def how_old(age_in_seconds)
-  Float(age_in_seconds) / 60 / 60 / 24 / 365
-end
- 
-age_in_seconds = 979_000_000
-puts "You are %.2f years old" % how_old(age_in_seconds)
--- 4e_minutes_in_a_year.rb
+++
@@ -1,3 +0,0 @@
-minutes = 60 * 24 * 365
- 
-puts "There are #{minutes} minutes in a year."
